8ed25cf07d3d0390ea6fec248bb6fcbf97389299
yogain123 <yogendrasaxena56@gmail.com>
2019-12-24 22:00:15 +0530

    added missing files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Twitter from "./component/Twitter";
+import Footer from "./component/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h3 style={{ textAlign: "center", marginTop: "60px" }}>
+        Finding Twitter Mutual Friends of 2 Users
+      </h3>
+      <Twitter />
+      <Footer />
     </div>
   );
 }
