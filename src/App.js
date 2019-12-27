beffe17f613cbf63402f297abdd090d03e469fcc
yogain123 <yogendrasaxena56@gmail.com>
2019-12-27 17:04:45 +0530

    Updated heading

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="container">
       <h3 style={{ textAlign: "center", marginTop: "60px" }}>
-        Finding Twitter Mutual Friends of 2 Users
+        Mutual Friend Finder - Twitter
       </h3>
       <Twitter />
       <Footer />
